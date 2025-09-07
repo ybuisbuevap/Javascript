e461b162ef336cec763d1e1e408ac062eb803929
Devang Singh <prakharsingh30072@gmail.com>
2025-09-07 14:23:02 +0000

    type

--- basics/variable.js
+++ basics/variable.js
@@ -4,3 +4,11 @@ let accountName = 1223;
 
 console.log(accountId);
 console.log(accountEmail);
+console.table([accountId, accountEmail , accountName]);
+
+console.log("helo");
+// alert("hello");
+console.log("bye");
+console.log(typeof 3);
+
+
